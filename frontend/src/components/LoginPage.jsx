7d0cee0ebe9c9dd9732c38583ea4779a55d6d7a8
naveenv0906 <naveenv0906@gmail.com>
2025-10-22 11:35:30 +0530

    bug fix for otp api url

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import LoadingSpinner from './LoadingSpinner';
 import { toast } from 'react-toastify';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4062';
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4062';
 
 const LoginPage = () => {
     const navigate = useNavigate();
