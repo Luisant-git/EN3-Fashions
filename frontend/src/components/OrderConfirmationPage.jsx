a95e31cf952dc22f151b038b46aae71a35fd3511
ragul.se21 <ragul.se21@bitsathy.ac.in>
2025-12-02 11:36:02 +0530

    bug fix

--- frontend/src/components/OrderConfirmationPage.jsx
+++ frontend/src/components/OrderConfirmationPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const OrderConfirmationPage = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const order = location.state?.order;
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div className="order-confirmation-page">
             <div className="confirmation-box">
